cf658cc0021fe8ce8fa4fecb039f749d6b80390f
Robs <robson.npc@gmail.com>
2024-06-03 10:55:02 -0300

    Inserção da Lista readNames
    Inserido uma lista para armazenar os nomes dos usuários de modo estástico.

--- main.kt
+++ main.kt
@@ -1,3 +1,10 @@
+//val -> declaração de variável somente leitura (constante)
+//var -> declaração de variável mutável
+//listOf -> criação de uma lista somente leitura
+//MutableListOf -> criação de uma lista mutável
+
+val radName = listOf("Davi", "João", "André")
+
 fun main() {
-    println("Hello, world!")
+    println("Hello, $name!")
 }
